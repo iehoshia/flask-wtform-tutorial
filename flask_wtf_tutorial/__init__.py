1f56afe2bd59939e9af9fb57f834ec94ba7607b0
iehoshia <jepgez@gmail.com>
2020-03-08 11:14:56 -0600

    SQLAlchemy

--- flask_wtf_tutorial/__init__.py
+++ flask_wtf_tutorial/__init__.py
@@ -3,18 +3,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
-    """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
     app.config['RECAPTCHA_PUBLIC_KEY'] = 'iubhiukfgjbkhfvgkdfm'
     app.config['RECAPTCHA_PARAMETERS'] = {'size': '100%'}
-    db = SQLAlchemy(app)
-	migrate = Migrate(app, db)
+
+    db.init_app(app)
+    migrate.init_app(app,db)
 
     with app.app_context():
-        # Import parts of our application
         from . import routes, models
-
         return app
